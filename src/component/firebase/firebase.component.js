5af5958424d836365ab5265b23d694ad31cabfac
n-busuttil <nbusuttil@norsys.fr>
2018-01-29 16:56:25 +0100

    use reactfire

--- src/component/firebase/firebase.component.js
+++ src/component/firebase/firebase.component.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB__6jacj_HLNPFAD0AzPp2T0f31Zsil_Q',
+  authDomain: 'morgane-comptability.firebaseapp.com',
+  databaseURL: 'https://morgane-comptability.firebaseio.com/',
+  projectId: 'morgane-comptability',
+  storageBucket: 'morgane-comptability.appspot.com',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 class Firebase extends Component {
-  saveData = () => {
+  componentWillMount() {
+    this.firebaseRef = firebase.database().ref('data/datas');
+    this.firebaseRef.limitToLast(25).on('value', dataSnapshot => {
+      const items = [];
+      dataSnapshot.forEach(childSnapshot => {
+        const item = childSnapshot.val();
+        item['.key'] = childSnapshot.key;
+        items.push(item);
+      });
+
+      this.setState({
+        items,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.firebaseRef.off();
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
     const { createFirebaseDataAction, consultations, filtering, charges } = this.props;
     const datas = { consultations, filtering, charges };
 
-    createFirebaseDataAction(datas);
+    this.firebaseRef.push(datas);
   };
+  /*saveData = () => {
+    const { createFirebaseDataAction, consultations, filtering, charges } = this.props;
+    const datas = { consultations, filtering, charges };
+
+    // createFirebaseDataAction(datas);
+    firebase.push('datas', datas);
+  };*/
 
   render() {
     return (
       <div>
-        <button onClick={this.saveData}>Sauvegarder</button>
+        <button onClick={this.handleSubmit}>Sauvegarder</button>
       </div>
     );
   }
--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyB__6jacj_HLNPFAD0AzPp2T0f31Zsil_Q',
-  authDomain: 'morgane-comptability.firebaseapp.com',
-  databaseURL: 'https://morgane-comptability.firebaseio.com',
-  projectId: 'morgane-comptability',
-  storageBucket: 'morgane-comptability.appspot.com',
-  messagingSenderId: '792515273374',
-};
