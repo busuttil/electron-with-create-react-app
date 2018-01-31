9fa1b7f4250874bb0c33671efdd85391f91339a2
n-busuttil <nbusuttil@norsys.fr>
2018-01-31 15:21:37 +0100

    prod app

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ import App from './App';
 
 import localStorage from './redux/middlewares/localStorage.middleware';
 
-// eslint-disable-next-line
-const reduxDebugger = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const store = createStore(reducer, reduxDebugger, applyMiddleware(logger, thunk, localStorage));
+const store = createStore(reducer, applyMiddleware(logger, thunk, localStorage));
 
 ReactDOM.render(
   <Provider store={store}>
